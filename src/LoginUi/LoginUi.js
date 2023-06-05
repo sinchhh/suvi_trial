e15a35e4bcac301e1c9c01c89150ccde9e037175
Sinchana Hebbar <sinchanhebbar15@gmail.com>
2023-06-05 13:39:30 +0530

    3 commit

--- src/LoginUi/LoginUi.js
+++ src/LoginUi/LoginUi.js
@@ -5,48 +5,45 @@ import pass from "./../image/lock.png";
 // import bg from "./../image/login_bgnew.png"
 function LoginUi() {
   return (
+    
     <div className='img'>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+
     <div className='suvi'>
       <p>SUVIDHA</p>
-    </div>  
+    </div> 
     <div className="main" >
      
      <div className="sub-main">
        <div>
-         {/* <div className="imgs">
-           {/* <div className="container-image">
-             <img src={profile} alt="profile" className="profile"/>
-
-           </div> 
-
-
-         </div> */}
          <div className='title'>
            <h1>LOGIN</h1>
            <div>
              <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="email" className="name"/>
+             <input type="text" placeholder="email" className="name"/><br></br>
            </div>
            <div className="second-input">
              <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="password" className="name"/>
+             <input type="password" placeholder="password" className="name"/><br></br>
            </div>
+
           <div className="login-button">
           <button>Login</button>
           </div>
-           
-            <p className="link">
+       
+           <p className="link">
               <a href="#">Forgot password?</a> 
               <br></br>
               <a href="#">Sign Up</a>
             </p>
+            <br></br>
            
  
          </div>
        </div>
        
 
-     </div>
+      </div> 
     </div>
     </div>
   );
